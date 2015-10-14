dad34c06b2d364976cdeeae2d427a06893b794c5
infiniteperplexity <m543015@CNHARAM3584750.cs.msds.kp.org>
2015-10-14 09:29:09 -0700

    adopted his pre-11 refactor

--- assets/game.js
+++ assets/game.js
@@ -1,79 +1,72 @@
-//http://www.codingcookies.com/2013/04/30/building-a-roguelike-in-javascript-part-6/
-
 var Game =  {
 	_display: null,
-	_currentScreen: null,
-	_screenWidth: 80,
-	_screenHeight: 24,
+    _currentScreen: null,
+    _screenWidth: 80,
+    _screenHeight: 24,
 	init: function() {
-		// Any necessary initialization will go here.
-		this._display = new ROT.Display({width: this._screenWidth,
-										 height: this._screenHeight + 1});
-		// Create a helper function for binding to an event
-		// and making it send it to the screen
-		var game = this; // So that we don't lose this
-		var bindEventToScreen = function(event) {
-			window.addEventListener(event, function(e) {
-				// When an event is received, send it to the
-				// screen if there is one
-				if (game._currentScreen !== null) {
-					// Send the event type and data to the screen
-					game._currentScreen.handleInput(event, e);
-					// Clear the screen
-					game._display.clear();
-					// Render the screen
-					game._currentScreen.render(game._display);
-				}
-			});
-		}
-		// Bind keyboard input events
-		bindEventToScreen('keydown');
-		//bindEventToScreen('keyup');
-		bindEventToScreen('keypress');
-		
-	},
+        // Any necessary initialization will go here.
+        this._display = new ROT.Display({width: this._screenWidth,
+                                         height: this._screenHeight + 1});
+        // Create a helper function for binding to an event
+        // and making it send it to the screen
+        var game = this; // So that we don't lose this
+        var bindEventToScreen = function(event) {
+            window.addEventListener(event, function(e) {
+                // When an event is received, send it to the
+                // screen if there is one
+                if (game._currentScreen !== null) {
+                    // Send the event type and data to the screen
+                    game._currentScreen.handleInput(event, e);
+                }
+            });
+        };
+        // Bind keyboard input events
+        bindEventToScreen('keydown');
+        //bindEventToScreen('keyup');
+        bindEventToScreen('keypress');
+    },
 	getDisplay: function() {
 		return this._display;
 	},
 	getScreenWidth: function() {
-		return this._screenWidth;
+    return this._screenWidth;
 	},
 	getScreenHeight: function() {
-		return this._screenHeight;
+	    return this._screenHeight;
 	},
+    refresh: function() {
+        // Clear the screen
+        this._display.clear();
+        // Render the screen
+        this._currentScreen.render(this._display);
+    },
 	switchScreen: function(screen) {
-		// If we had a screen before, notify it that we exited
-		if (this._currentScreen !== null) {
-			this._currentScreen.exit();
-		}
-		// Clear the display
-		this.getDisplay().clear();
-		// Update our current screen, notify it we entered
-		// and then render it
-		this._currentScreen = screen;
-		if (!this._currentScreen !== null) {
-			this._currentScreen.enter();
-			this.refresh();
-		}
-	},
-	refresh: function() {
-		// Clear the screen
-		this._display.clear();
-		// Render the screen
-		this._currentScreen.render(this._display);
-	},
-}
+        // If we had a screen before, notify it that we exited
+        if (this._currentScreen !== null) {
+            this._currentScreen.exit();
+        }
+        // Clear the display
+        this.getDisplay().clear();
+        // Update our current screen, notify it we entered
+        // and then render it
+        this._currentScreen = screen;
+        if (!this._currentScreen !== null) {
+            this._currentScreen.enter();
+            this.refresh();
+        }
+    }
+};
 
 window.onload = function() {
-	// Check if rot.js can work on this browser
-	if (!ROT.isSupported()) {
-		alert("The rot.js library isn't supported by your browser.");
-	} else {
-		// Initialize the game
-		Game.init();
-		// Add the container to our HTML page
-		document.body.appendChild(Game.getDisplay().getContainer());
-		// Load the start screen
-		Game.switchScreen(Game.Screen.startScreen);
-	}
-}
+    // Check if rot.js can work on this browser
+    if (!ROT.isSupported()) {
+        alert("The rot.js library isn't supported by your browser.");
+    } else {
+        // Initialize the game
+        Game.init();
+        // Add the container to our HTML page
+        document.body.appendChild(Game.getDisplay().getContainer());
+        // Load the start screen
+        Game.switchScreen(Game.Screen.startScreen);
+    }
+};
--- assets/items.js
+++ assets/items.js
@@ -1,13 +1,13 @@
 Game.ItemRepository = new Game.Repository('items', Game.Item);
 
 Game.ItemRepository.define('apple', {
-	name: 'apple',
-	character: '%',
-	foreground: 'red'
+    name: 'apple',
+    character: '%',
+    foreground: 'red'
 });
 
 Game.ItemRepository.define('rock', {
-	name: 'rock',
-	character: '*',
-	foreground: 'white'
+    name: 'rock',
+    character: '*',
+    foreground: 'white'
 });
